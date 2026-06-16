2b7ed74838710740acce290a5b53da296fdc83fe
Rishita-22 <rishitapatri@gmail.com>
2026-06-16 23:49:23 +0530

    Temporary auto superuser fix for Render login

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 def login_view(request):
+
+    def create_default_user():
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            password="admin123"
+        )
+
+    create_default_user()  # TEMP ONLY FOR RENDER DEBUG
 
     if request.method == 'POST':
 
